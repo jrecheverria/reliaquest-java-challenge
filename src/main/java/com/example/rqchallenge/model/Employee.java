02cef17b5824f71e8738129618a5674fdf172e1a
Ruben Echeverria <javier.ruben.echeverria@gmail.com>
2024-07-24 23:38:15 -0400

    feat: Create employee model class

---
+++ src/main/java/com/example/rqchallenge/model/Employee.java
@@ -0,0 +1,9 @@
+package com.example.rqchallenge.model;
+
+public class Employee {
+    private int id;
+    private String name;
+    private int salary;
+    private int age;
+    private String profileImage;
+}

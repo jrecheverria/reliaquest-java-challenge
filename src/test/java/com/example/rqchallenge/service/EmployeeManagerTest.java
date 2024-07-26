e04be770c3fd1d1e2c40b4da4f26e51dd52c855b
Ruben Echeverria <javier.ruben.echeverria@gmail.com>
2024-07-26 15:26:05 -0400

    feat: Completed unit test for string search

--- src/test/java/com/example/rqchallenge/service/EmployeeManagerTest.java
+++ src/test/java/com/example/rqchallenge/service/EmployeeManagerTest.java
@@ -16,10 +16,12 @@ class EmployeeManagerTest {
     void setUp() {
         employeeManager = new EmployeeManager();
         employeeList = Arrays.asList(
-                new Employee("1", "Jon Snow", "100000", "28", "Castle_Black.jpg"),
+                new Employee("1", "Jon Snow", "100000", "28", "CastleBlack.jpg"),
                 new Employee("2", "Arya Stark", "120000", "18", "Winterfell.jpg"),
-                new Employee("3", "Tyrion Lannister", "110000", "40", "Casterly_Rock.jpg"),
-                new Employee("4", "Daenerys Targaryen", "130000", "25", "Dragonstone.jpg")
+                new Employee("3", "Tyrion Lannister", "110000", "40", "CasterlyRock.jpg"),
+                new Employee("4", "Daenerys Targaryen", "130000", "25", "Dragonstone.jpg"),
+                new Employee("5", "Rob Stark", "100000", "30", "Winterfell.jpg"),
+                new Employee("6", "Ned Stark", "100000", "50", "Winterfell.jpg")
         );
         employeeManager.populateEmployeeData(employeeList);
     }
@@ -41,6 +43,18 @@ class EmployeeManagerTest {
                 "Daenerys Targaryen", "Arya Stark", "Tyrion Lannister", "Jon Snow"
         );
         assertEquals(expectedNames, topEmployees);
+    }
+
+    @Test
+    void testGetEmployeesByNameSearch() {
+        //Using search string 'st' should fetch some Lannisters and Starks
+        List<Employee> result = employeeManager.getEmployeesByNameSearch("St");
+
+        assertEquals(4, result.size());
+        assertEquals("Tyrion Lannister", result.get(0).getName());
+        assertEquals("Ned Stark", result.get(1).getName());
+        assertEquals("Rob Stark", result.get(2).getName());
+        assertEquals("Arya Stark", result.get(3).getName());
     }
 
 }

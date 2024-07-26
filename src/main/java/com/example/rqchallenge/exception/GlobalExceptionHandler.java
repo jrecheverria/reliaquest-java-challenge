dce0c82efd79dbffde49b63b7706036bc5e61742
Ruben Echeverria <javier.ruben.echeverria@gmail.com>
2024-07-26 09:42:38 -0400

    feat: Another exception type for handling IO errors

--- src/main/java/com/example/rqchallenge/exception/GlobalExceptionHandler.java
+++ src/main/java/com/example/rqchallenge/exception/GlobalExceptionHandler.java
@@ -8,8 +8,23 @@ import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.client.RestClientException;
 
+import java.io.IOException;
+
 @ControllerAdvice
 public class GlobalExceptionHandler {
 
     private static final Logger logger = LoggerFactory.getLogger(GlobalExceptionHandler.class);
+
+    //Exception for when calling the external API
+    @ExceptionHandler(RestClientException.class)
+    public ResponseEntity<String> handleRestClientException(RestClientException ex) {
+        logger.error("Rest client error: {}", ex.getMessage());
+        return new ResponseEntity<>("An error occurred while calling an external service.", HttpStatus.INTERNAL_SERVER_ERROR);
+    }
+
+    //Exception for processing a request
+    public ResponseEntity<String> handleIOException(IOException ex) {
+        logger.error("IOException occurred: {}", ex.getMessage());
+        return new ResponseEntity<>("An error occurred while processing the request", HttpStatus.INTERNAL_SERVER_ERROR);
+    }
 }

926bbe70759567e155c8b35c8af792989fd45944
Ruben Echeverria <javier.ruben.echeverria@gmail.com>
2024-07-26 09:39:18 -0400

    feat: Basic template for GlobalExceptionHandler

--- src/main/java/com/example/rqchallenge/exception/GlobalExceptionHandler.java
+++ src/main/java/com/example/rqchallenge/exception/GlobalExceptionHandler.java
@@ -1,4 +1,15 @@
 package com.example.rqchallenge.exception;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.client.RestClientException;
+
+@ControllerAdvice
 public class GlobalExceptionHandler {
+
+    private static final Logger logger = LoggerFactory.getLogger(GlobalExceptionHandler.class);
 }
